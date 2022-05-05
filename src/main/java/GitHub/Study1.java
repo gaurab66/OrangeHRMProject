95d9da8bbdbb0a9c0dbd76b295bd7942858145f2
Lenovo <Lenovo@DESKTOP-4DEEQQQ>
2022-05-05 23:51:27 +0530

    Code Committed by Gaurav...

---
+++ src/main/java/GitHub/Study1.java
@@ -0,0 +1,24 @@
+package GitHub;
+
+public class Study1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("GitHUb Preperation is Start");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/GitHub/Study2.java
@@ -0,0 +1,21 @@
+package GitHub;
+
+public class Study2 {
+	
+	
+	
+public static void main(String[] args) {
+	
+	System.out.println("GitHub preparation is mid");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
